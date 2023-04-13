786536b2a66cb1a94a174263a5bffb919755fffd
Lee <lee.jensen@agilix.com>
2023-04-13 13:23:55 -0600

    fix(deploy) Run script with an interactive shell so it gets the right environment

--- deploy.sh
+++ deploy.sh
@@ -42,6 +42,7 @@ scp -r -i "$key" dist/* ubuntu@$hostname:services/$service
 # Step 4
 printf "\n----> Deploy the service on the target\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
+bash -i
 cd services/${service}
 npm install
 pm2 restart ${service}
